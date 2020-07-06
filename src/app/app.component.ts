4db01a33927f1191e1c7c3a07569d9e9ab68f1e8
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-07-06 14:49:30 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Sorting-Visualizer-App';
+  loaderviewing:boolean = true;
+  ngOnInit():void{
+    this.loaderviewing = false;
+  }
+
 }
